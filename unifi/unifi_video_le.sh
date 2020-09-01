d304bb8427a52d9ac8e3b7a0090e18ea8c7bae8c
Drew Gauderman <drew@hyak.co>
2020-09-01 12:30:46 -0700

    updated unifi video

--- unifi/unifi_video_le.sh
+++ unifi/unifi_video_le.sh
@@ -18,34 +18,22 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #--------------------------------------------------
-# Unifi Video Lets Encrypt. v1.0.0
+# Unifi Video Lets Encrypt. v1.0.1
+# To install: https://bleepingmachines.com/unifi-video-and-letsencrypt/
 #
-# To install:
-# ---------------------
-#
-# 1) install certbot (https://certbot.eff.org/instructions)
-#   cd /usr/local/sbin
-#   sudo wget https://dl.eff.org/certbot-auto
-#   sudo chmod a+x certbot-auto
-#   sudo ./certbot-auto certonly
-#
-# 2) save the script somewhere on the file system.
-#    sudo nano /etc/unifi_video_le.sh
-#       (paste all the code (this entire file) and save)
-#
-# 3) setup execute permissions:
-#   sudo chmod a+x /etc/unifi_video_le.sh
-#
-# 4) manually run the script:
-#   sudo /etc/unifi_video_le.sh
-#
-# 5) Optional, create a cronjob to run once a month (https://www.geeksforgeeks.org/how-to-setup-cron-jobs-in-ubuntu/):
-#   crontab -e
-#     0 0 1 * * /etc/unifi_video_le.sh >/dev/null 2>&1
+# Release Notes:
+#   v1.0.1 - Added UNIFIPATH for custom install paths
+#   v1.0.0 - Inital Release
 #--------------------------------------------------
 
-# Set the Domain name, that points to your NVR, a valid DNS entry must exist
-DOMAIN="sub.yourdomain.com"
+# Set the UNIFIDOMAIN name, that points to your NVR, a valid DNS entry must exist
+UNIFIDOMAIN="nvr.yourdomain.com"
+
+# Folder UNIFIPATH to your Unifi Video data. Depends on how it was installed.
+# most common
+UNIFIPATH="/var/lib/unifi-video"
+# least common
+#UNIFIPATH="/usr/lib/unifi-video/data"
 
 # NO NEED TO DO NOT EDIT BELOW --------------
 
@@ -53,19 +41,19 @@ DOMAIN="sub.yourdomain.com"
 service unifi-video stop
 
 # Enable custom certificates in the system.properties for Unifi Video
-grep -qxF 'ufv.custom.certs.enable=true' /var/lib/unifi-video/system.properties || echo "ufv.custom.certs.enable=true" >>/var/lib/unifi-video/system.properties
+grep -qxF 'ufv.custom.certs.enable=true' ${UNIFIPATH}/system.properties || echo "ufv.custom.certs.enable=true" >>${UNIFIPATH}/system.properties
 
 #backup previous keystore
-cp /var/lib/unifi-video/keystore /var/lib/unifi-video/keystore.backup.$(date +%F_%R)
+cp ${UNIFIPATH}/keystore ${UNIFIPATH}/keystore.backup.$(date +%F_%R)
 
 #Renew the certificate
 sudo certbot-auto renew --quiet --no-self-upgrade
 
 # Convert cert to PKCS12 format
-sudo openssl pkcs12 -export -inkey /etc/letsencrypt/live/${DOMAIN}/privkey.pem -in /etc/letsencrypt/live/${DOMAIN}/fullchain.pem -out /etc/letsencrypt/live/${DOMAIN}/fullchain.p12 -name airvision -password pass:ubiquiti
+sudo openssl pkcs12 -export -inkey /etc/letsencrypt/live/${UNIFIDOMAIN}/privkey.pem -in /etc/letsencrypt/live/${UNIFIDOMAIN}/fullchain.pem -out /etc/letsencrypt/live/${UNIFIDOMAIN}/fullchain.p12 -name airvision -password pass:ubiquiti
 
 # Import certificate
-sudo keytool -importkeystore -deststorepass ubiquiti -destkeypass ubiquiti -destkeystore /var/lib/unifi-video/keystore -srckeystore /etc/letsencrypt/live/${DOMAIN}/fullchain.p12 -srcstoretype pkcs12 -srcstorepass ubiquiti -alias airvision -noprompt
+sudo keytool -importkeystore -deststorepass ubiquiti -destkeypass ubiquiti -destkeystore ${UNIFIPATH}/keystore -srckeystore /etc/letsencrypt/live/${UNIFIDOMAIN}/fullchain.p12 -srcstoretype pkcs12 -srcstorepass ubiquiti -alias airvision -noprompt
 
 # Start the UniFi Video service
 service unifi-video start
